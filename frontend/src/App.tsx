89745dde414f1fb588790e7d16d0247c63d4642d
ThaoHien121 <3t.ahin.thaohientruongthi@gmail.com>
2026-03-21 14:10:10 +0700

    Remove Dashboard route from App component
    Bản ban đầu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CustomerCreatePage from "./pages/CustomerCreatePage";
 import CostumeCreatePage from "./pages/CostumeCreatePage";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-
-        <Route path="/dashboard" element={<Dashboard />} />
-
         <Route path="/customers/create" element={<CustomerCreatePage />} />
         <Route path="/costumes/create" element={<CostumeCreatePage />} />
       </Routes>
