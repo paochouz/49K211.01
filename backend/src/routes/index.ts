6bd88fd892e95e3d9d9bb3250e3ba62612ee8f46
Tường Vi <125167432+behatiu@users.noreply.github.com>
2026-04-09 22:06:24 +0700

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
+import { createKhachHang } from "../controllers/adddonthueController";
 import { authRoutes } from "./authRoutes";
-import { customerRoutes } from "./customerRoutes";
 import { costumeRoutes } from "./costumeRoutes";
+import { customerRoutes } from "./customerRoutes";
+import { dashboardRoutes } from "./dashboardRoutes";
+import { donThueRoutes } from "./donThueRoutes";
 
 export const routes = Router();
 
 routes.use("/auth", authRoutes);
 routes.use("/customers", customerRoutes);
 routes.use("/costumes", costumeRoutes);
-
+routes.use("/dashboard", dashboardRoutes);
+routes.use("/don-thue", donThueRoutes);
+routes.post("/khach-hang/don-thue", createKhachHang);
